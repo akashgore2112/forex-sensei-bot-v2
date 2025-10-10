a40ee83d4220b3c2de8ebd2af462ba616a09ea1f
akashgore2112 <akashgore2112@gmail.com>
2025-10-10 20:05:16 +0400

    Create config.js

---
+++ src/strategies/meanReversion/config.js
@@ -0,0 +1,8 @@
+export const MR_CONFIG = {
+  rsiLow: 25,
+  rsiHigh: 75,
+  adxMax: 20,
+  levelTolBps: 15,   // 0.15% distance to zone
+  atrSL: 1.5,        // SL = 1.5 * ATR(14)
+  rr: 1.5,           // TP = SL * rr
+};
